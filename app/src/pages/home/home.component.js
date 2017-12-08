12345784f1afde2aa3c0792bcf386ad80c8a12d8
abelade <rademoroti@gmail.com>
2017-12-08 15:15:23 +0000

    Update home.component.js

--- app/src/pages/home/home.component.js
+++ app/src/pages/home/home.component.js
@@ -38,7 +38,7 @@ function HomeController() {
         ];
     };
 
-    $ctrl.addNote = function($scope) {
+    $ctrl.addNote = function() {
 
 
         var allNotes = document.getElementsByTagName("note-component");
@@ -51,23 +51,31 @@ function HomeController() {
             document.documentElement.scrollLeft:document.body.scrollLeft;
         elementTop = rect.top+scrollTop+480;
         elementLeft = rect.left+scrollLeft+40;
-        $scope.addNote = function() {
-            $scope.newNote = {};
-            $scope.position = {
+
+        $ctrl.addNote = function() {
+            console.log("not added");
+            $ctrl.newNote = {};
+            $ctrl.position = {
                 x: elementTop,
                 y: elementLeft
             };
-            $scope.color = '#ffd6bc';
-            $scope.header = "Note 1";
-            $scope.text = "Test note 1";
-            $scope.newNote.createdOn = Date.now();
-            $scope.newNote.text = ' ';
-            $scope.newNote.edit = true;
-            $scope.notes.push($scope.newNote);
-            $scope.newNote = {};
-            $scope.noteText = ''; //clear the input after adding
+            $ctrl.color = '#ffd6bc';
+            $ctrl.header = "Note 1";
+            $ctrl.text = "Test note 1";
+            $ctrl.newNote.createdOn = Date.now();
+            $ctrl.newNote.text = ' ';
+            $ctrl.newNote.edit = true;
+            $ctrl.notes.push($ctrl.newNote);
+            $ctrl.newNote = {};
+            $ctrl.noteText = ''; //clear the input after adding
             // localStorage.setItem('notes', JSON.stringify($scope.notes));
         }
+
+        $ctrl.delete = function (i) {
+            var r = confirm("Are you sure you want to delete this note?");
+            if (r == true)
+                $ctrl.notes.splice(i, 1);
+        };
     }
 
 
